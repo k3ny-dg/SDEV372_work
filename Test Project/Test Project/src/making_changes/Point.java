411570ad02152281bc078095129ba6228e7d5ea2
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-01-03 21:14:35 -0800

    Added equals() and hashCode() method the Point class.

--- Test Project/Test Project/src/making_changes/Point.java
+++ Test Project/Test Project/src/making_changes/Point.java
@@ -1,5 +1,7 @@
 package making_changes;
 
+import java.util.Objects;
+
 public class Point
 {
     private double x;
@@ -23,6 +25,25 @@ public class Point
     public void setY(double y)
     {
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(x, y);
     }
 
     public Point(double x, double y)
