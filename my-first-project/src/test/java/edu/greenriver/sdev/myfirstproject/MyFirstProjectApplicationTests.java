589e003c1e8414efb64789b2199a8320d7741d3d
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-01-09 13:37:45 -0800

    Added initial files for the project.

---
+++ my-first-project/src/test/java/edu/greenriver/sdev/myfirstproject/MyFirstProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
