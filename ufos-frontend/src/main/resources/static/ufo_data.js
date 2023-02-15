c72f8fee2d07d1bbb755a079f8170eb2e34b9239
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-02-14 21:56:53 -0800

    Added a table that displays all UFO records using JS.

--- ufos-frontend/src/main/resources/static/ufo_data.js
+++ ufos-frontend/src/main/resources/static/ufo_data.js
@@ -1,8 +1,10 @@
-window.onload = loadData;
+window.onload = function (){
+    loadData();
+}
 
 function loadData()
 {
-    let uri= "http://localhost:8080/ufos";
+    let uri= "http://localhost:8080/ufos/all";
     let params = {
         method: "get",
         mode: "cors"
@@ -15,10 +17,63 @@ function loadData()
         .then(function (json){
         console.log(json);
         showData(json);
-});
+    });
 }
 
 function showData(ufos)
 {
-    let list = document.getElementById("list");
+    let list = document.getElementById("ufos");
+    let table = document.getElementById("ufo-table");
+
+    for (let i = 0; i < ufos.length; i++) {
+
+        let ufo = ufos[i];
+
+        // let li = document.createElement("li");
+        // li.innerHTML = ufo.id;
+        // console.log("id =" + ufo.id);
+        // list.appendChild(li);
+
+        let tr = document.createElement("tr");
+        table.appendChild(tr);
+        table.appendChild(tr);
+        table.appendChild(tr);
+        table.appendChild(tr);
+        table.appendChild(tr);
+        table.appendChild(tr);
+
+        let id_td = document.createElement("td");
+        let shape_td = document.createElement("td");
+        let description_td = document.createElement("td");
+        let encounter_td = document.createElement("td");
+        let edit_td = document.createElement("td");
+        let del_td = document.createElement("td");
+
+        id_td.className = "id";
+        shape_td.className = "shape";
+        description_td.className = "description";
+        encounter_td.className = "encounter";
+        edit_td.className = "edit";
+        del_td.className = "del";
+
+        id_td.innerHTML = ufo.id;
+        shape_td.innerHTML = ufo.shape;
+        description_td.innerHTML = ufo.description;
+        encounter_td.innerHTML = ufo.encounterLength;
+        edit_td.innerHTML = "Edit";
+        del_td.innerHTML = "Delete";
+
+        console.log(ufo.id);
+        console.log(ufo.shape);
+        console.log(ufo.description);
+        console.log(ufo.encounterLength);
+
+
+        tr.appendChild(id_td);
+        tr.appendChild(shape_td);
+        tr.appendChild(description_td);
+        tr.appendChild(encounter_td);
+        tr.appendChild(edit_td);
+        tr.appendChild(del_td);
+    }
 }
---
+++ ufos-frontend/src/test/java/edu/greenriver/sdev/ufosfrontend/UfosFrontendApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.ufosfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfosFrontendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
